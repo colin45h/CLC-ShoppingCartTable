15fa37a2698f1f0d1c3b83b2b6b2e07e099e94de
Colin Hehn <colin45h@yahoo.com>
2020-11-18 10:41:47 -0600

    Initial Commit

---
+++ shoppingCartApp_HehnCTests/shoppingCartApp_HehnCTests.swift
@@ -0,0 +1,34 @@
+//
+//  shoppingCartApp_HehnCTests.swift
+//  shoppingCartApp_HehnCTests
+//
+//  Created by Tiger Coder on 11/18/20.
+//  Copyright © 2020 clc.hehn. All rights reserved.
+//
+
+import XCTest
+@testable import shoppingCartApp_HehnC
+
+class shoppingCartApp_HehnCTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
